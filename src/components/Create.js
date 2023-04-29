b5ece9e2813a8cf2353ac81a05a257b1bb06bc3a
Harn1sh <41642384+Harn1sH@users.noreply.github.com>
2023-04-30 00:16:52 +0530

    .

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import { Button, IconButton, TextField } from "@mui/material";
+import { Button, TextField } from "@mui/material";
 import { PhotoCamera } from "@mui/icons-material";
+import axios from "axios";
 
 const Create = ({ foods, setFoods }) => {
   const [image, setImage] = useState("");
@@ -10,8 +11,13 @@ const Create = ({ foods, setFoods }) => {
   const [type, setType] = useState("");
   const [tags, settags] = useState("");
 
-  const saveFood = () => {
-    console.log(foods);
+  const uploadImage = (e) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.append("myImage", image);
+    axios.post("url", formData).then((res) => {});
+
     setFoods([
       ...foods,
       {
@@ -23,76 +29,89 @@ const Create = ({ foods, setFoods }) => {
         Image: image,
       },
     ]);
-    console.log(foods);
+    setImage("");
+    setName("");
+    setCusine("");
+    setRests("");
+    setType("");
+    settags("");
   };
 
   return (
     <div className="flex justify-center items-center h-screen ">
-      <div className="flex flex-col gap-y-4 border-2 h-[600px] w-[300px] md:w-[600px] md:h-[500px] justify-center items-center border-zinc-800">
-        <TextField
-          label="Dish Name"
-          className="md:w-[500px] "
-          value={name}
-          onChange={(e) => {
-            setName(e.target.value);
-          }}
-        />
-        <TextField
-          label="Cusine"
-          className="md:w-[500px] "
-          value={cusine}
-          onChange={(e) => {
-            setCusine(e.target.value);
-          }}
-        />
-        <TextField
-          label="Restrictions"
-          className="md:w-[500px] "
-          value={restr}
-          onChange={(e) => {
-            setRests(e.target.value);
-          }}
-        />
-        <TextField
-          label="Type"
-          className="md:w-[500px] "
-          value={type}
-          onChange={(e) => {
-            setType(e.target.value);
-          }}
-        />
-        <TextField
-          label="Tags"
-          className="md:w-[500px] "
-          value={tags}
-          onChange={(e) => {
-            settags(e.target.value);
-          }}
-        />
-        <div>
-          <Button
-            variant="contained"
+      <form onSubmit={uploadImage}>
+        <div className="flex flex-col gap-y-4 border-2 h-[600px] w-[300px] md:w-[600px] md:h-[500px] justify-center items-center border-zinc-800">
+          <TextField
+            label="Dish Name"
+            className="md:w-[500px] "
+            value={name}
             onChange={(e) => {
-              console.log(e.target.files[0]);
+              setName(e.target.value);
             }}
-            color="primary"
-            component="label"
-            endIcon={<PhotoCamera />}
-          >
-            upload
-            <input hidden accept="image" multiple type="file" />
-          </Button>
+          />
+          <TextField
+            label="Cusine"
+            className="md:w-[500px] "
+            value={cusine}
+            onChange={(e) => {
+              setCusine(e.target.value);
+            }}
+          />
+          <TextField
+            label="Restrictions"
+            className="md:w-[500px] "
+            value={restr}
+            onChange={(e) => {
+              setRests(e.target.value);
+            }}
+          />
+          <TextField
+            label="Type"
+            className="md:w-[500px] "
+            value={type}
+            onChange={(e) => {
+              setType(e.target.value);
+            }}
+          />
+          <TextField
+            label="Tags"
+            className="md:w-[500px] "
+            value={tags}
+            onChange={(e) => {
+              settags(e.target.value);
+            }}
+          />
+          <div>
+            <Button
+              variant="contained"
+              disableElevation
+              onChange={(e) => {
+                var reader = new FileReader();
+                reader.readAsDataURL(e.target.files[0]);
+                reader.onload = () => {
+                  console.log(reader.result);
+                  setImage(reader.result);
+                };
+              }}
+              color="primary"
+              component="label"
+              endIcon={<PhotoCamera />}
+            >
+              upload
+              <input hidden accept="image" multiple type="file" />
+            </Button>
+            {"    "}
+            <Button
+              type="submit"
+              disableElevation
+              variant="contained"
+              color="success"
+            >
+              Done
+            </Button>
+          </div>
         </div>
-        <Button
-          variant="contained"
-          color="success"
-          onClick={() => {
-            saveFood();
-          }}
-        >
-          Done
-        </Button>
-      </div>
+      </form>
     </div>
   );
 };
